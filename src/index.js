5e58378cd46476f183e5fe2a8bfeb99c4d21ffe7
Erik Golden <erikgolden7@gmail.com>
2018-07-10 10:20:42 -0500

    finished demo, added clarification comments

--- src/index.js
+++ src/index.js
@@ -3,32 +3,3 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// React and Node
-// npm i --save express react react-dom
-
-// Babel
-// npm i --save babel-core babel-loader babel-preset-es2017 babel-preset-react
-
-// Webpack
-// npm i --save webpack webpack-dev-server webpack-dev-middleware
-
-// Webpack plugins
-// npm i --save html-webpack-plugin clean-webpack-plugin uglifyjs-webpack-plugin
-
-// Webpack loaders
-// npm i --save css-loader file-loader style-loader
-
-// npm i express react react-dom babel-core babel-loader env babel-preset-es2017 babel-preset-react webpack webpack-dev-server webpack-dev-middleware html-webpack-plugin clean-webpack-plugin uglifyjs-webpack-plugin css-loader file-loader style-loader
-
-// NEW DEPENDENCIES
-// npm i webpack webpack-cli babel-core babel-loader babel-preset-env react react-dom babel-preset-react html-webpack-plugin html-loader mini-css-extract-plugin css-loader webpack-dev-server --save-dev
-
-// yarn add webpack webpack-cli babel-core babel-loader babel-preset-env react react-dom babel-preset-react html-webpack-plugin html-loader mini-css-extract-plugin css-loader webpack-dev-server --save-dev
-
-// "test": "echo \"Error: no test specified\" && exit 1",
-// "dev": "webpack --mode development",
-// "build": "webpack --mode production",
-// "watch": "webpack --progress --colors --watch",
-// "start": "webpack-dev-server --progress",
-// "server": "nodemon server.js"
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,45 @@
 // We are using node's native package 'path'. For more info check out https://nodejs.org/api/path.html
-// const path = require('path');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const CleanWebpackPlugin = require('clean-webpack-plugin');
-// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+// Production mode enables all sorts of optimizations out of the box. Including minification, scope hoisting, tree-shaking and more.
+// Development mode on the other hand is optimized for speed and does nothing more than providing an un-minified bundle.
+
+// Webpack v4 no longer requires an entry or output, and can even be run without a webpack.config file!
+// It will look in ./src/index.js as the default entry point. Moreover, it will spit out the bundle in ./dist/main.js.
 module.exports = {
-  // represents our entry javascript file for the project.
-  // entry: path.resolve(__dirname, 'src/index.js'),
-  // Tells the dev server which port to run on, where to look for files, and compresses the files.
-  // devServer: {
-  //   contentBase: path.resolve(__dirname, 'dist'),
-  //   port: 3001,
-  //   compress: true
-  // },
+  // Tell the dev server which port to run on, where to look for files, and to compress files.
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 3005,
+    compress: true
+  },
+
   // Maps your compiled code back to your original source code.
-  // devtool: 'source-map',
-  // plugins: [
-  //   new CleanWebpackPlugin(['dist']),
-  //   // Tell webpack to use html plugin.
-  //   new HtmlWebpackPlugin({
-  //     template: path.resolve(__dirname, 'public/index.html')
-  //   }),
-  //   // Minifies the bundle.js file.
-  //   new UglifyJSPlugin()
-  // ],
+  devtool: 'source-map',
+
+  // Plugins
   plugins: [
+    // A webpack plugin to remove/clean your build folder(s) before building
+    new CleanWebpackPlugin(['dist']),
+    // Simplifies creation of HTML files to serve your webpack bundles
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: path.resolve(__dirname, './public/index.html'),
       filename: './index.html'
     }),
+    // This plugin extracts CSS into separate files. It creates a CSS file per JS file which contains CSS.
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[id].css'
     })
   ],
-  // Loaders configurations tell webpack to use loaders for specific file types.
+
+  // webpack enables use of loaders to preprocess files. This allows you to bundle any static resource.
   module: {
     rules: [
+      // HTML
       {
         test: /\.html$/,
         use: [
@@ -47,6 +49,7 @@ module.exports = {
           }
         ]
       },
+      // JAVASCRIPT
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -54,21 +57,16 @@ module.exports = {
           loader: 'babel-loader'
         }
       },
+      // CSS
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
+      // IMAGES
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loader: 'file-loader'
       }
     ]
   }
-
-  //  the output bundled file, which we have to include in our main HTML file called bundle.js
-  // output: {
-  //   path: path.resolve(__dirname, 'dist'),
-  //   filename: 'bundle.js',
-  //   publicPath: '/'
-  // }
 };
