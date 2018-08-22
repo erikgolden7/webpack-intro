27c0db34e088431c6e4cf5008138089789cc5e27
Erik Golden <erikgolden7@gmail.com>
2018-08-22 10:11:11 -0500

    added hot module replacement, comments

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 // Production mode enables all sorts of optimizations out of the box. Including minification, scope hoisting, tree-shaking and more.
 // Development mode on the other hand is optimized for speed and does nothing more than providing an un-minified bundle.
@@ -14,7 +15,8 @@ module.exports = {
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
     port: 3005,
-    compress: true
+    compress: true,
+    hot: true
   },
 
   // Maps your compiled code back to your original source code.
@@ -33,7 +35,9 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[id].css'
-    })
+    }),
+    // Hot module replacement allows your application to only update changed files. The page will no longer hard refresh.
+    new webpack.HotModuleReplacementPlugin()
   ],
 
   // webpack enables use of loaders to preprocess files. This allows you to bundle any static resource.
